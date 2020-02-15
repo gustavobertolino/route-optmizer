b8913c6be0a0d9a529996cdd2aa90dca412c9da4
Gustavo <gustavobertolino@gmail.com>
2020-02-15 12:22:08 -0300

    [opt-001] Add unit test setup and first basic server endpoint test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Route-opt, from express');
+    res.send('Route-opt, from express!');
 });
 
 app.listen(port, () => console.log(`Route-opt listening on port ${port}!`))
