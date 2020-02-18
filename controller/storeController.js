07657ac178515a9a33117db1728add635b1e0027
Gustavo <gustavobertolino@gmail.com>
2020-02-17 23:33:03 -0300

    [opt-001] Add entities, add controller implementation. Add service layer to modularize methods.

---
+++ controller/storeController.js
@@ -0,0 +1,71 @@
+import StoreService from '../service/StoreService.js';
+
+// Create and Save a new Store
+exports.create = async(req, res) => {
+    // Validate request
+    if(!req.body.name || !req.body.latitude || !req.body.longitude) {
+        return res.status(400).send({
+            message: "Store name and coordinates cannot be empty"
+        });
+    }
+
+    const storeDto = req.body;
+    const newStore = await StoreService.createStore(storeDto);
+    return res.json({newStore});
+};
+
+// Find a single store with an id
+exports.findOne = (req, res) => {
+    Store.findById(req.params.id)
+    .then(store => {
+        if(!store) {
+            return res.status(404).send({
+                message: "Store not found with id " + req.params.id
+            });            
+        }
+        res.send(store);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Store not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving store with id " + req.params.id
+        });
+    });
+};
+
+// Update a store identified by the id in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.name) {
+        return res.status(400).send({
+            message: "Store name and coordinates cannot be empty"
+        });
+    }
+
+    // Find store and update it with the request body
+    Note.findByIdAndUpdate(req.params.noteId, {
+        name: req.body.name,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude
+    }, {new: true})
+    .then(store => {
+        if(!store) {
+            return res.status(404).send({
+                message: "Store not found with id " + req.params.id
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Store not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating store with id " + req.params.id
+        });
+    });
+};
