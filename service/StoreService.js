037b0f5a760d4e76efe4127afd7b09d34ac3b173
Gustavo <gustavobertolino@gmail.com>
2020-02-29 18:44:57 -0300

    [opt-001] Adjust storeService and controller to return and receive a Promisse by means of await and async annotations

--- service/StoreService.js
+++ service/StoreService.js
@@ -1,15 +1,14 @@
 const Store = require('../model/Store.js');
-const Promise = require('bluebird');
 
-const createNewStore = async (storeDto) => {
-    try {
-        const newStore = new Store({
-            name: storeDto.name,
-            latitude: storeDto.latitude,
-            longitude: storeDto.longitude
-        })
-        return await newStore.save();
-    } catch (error) {
-        throw new Error(error);
-    } 
+exports.createNewStore = async (storeDto) => {
+    const newStore = new Store({
+        name: storeDto.name,
+        latitude: storeDto.latitude,
+        longitude: storeDto.longitude
+    });
+
+    await newStore.save()
+        .then(data => {return {success: true, result: data};})
+        .catch(error => {return {success: false, result: error};
+    });
 }
